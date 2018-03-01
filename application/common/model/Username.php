68f668c7024ee78e703ba1d2750bb2269bffd3f9
mahaiyin <mahaiyin@qq.com>
2018-03-01 15:34:43 +0800

    username model

---
+++ application/common/model/Username.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-01
+ * Time: 10:56
+ */
+namespace app\common\model;
+
+use think\Model;
+
+class Username extends Model
+{
+    protected $pk = 'id';
+}
