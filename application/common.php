2985b9aa16f042d5ebb1610794c857d2a5b87d0e
mahaiyin <mahaiyin@qq.com>
2018-03-01 17:11:07 +0800

    用户名正则

--- application/common.php
+++ application/common.php
@@ -15,3 +15,9 @@ if (!function_exists('isMobile')) {
         return preg_match('/^1[3456789]\d{9}$/', $mobile);
     }
 }
+if (!function_exists('isUsername')) {
+    function isUsername($username = '')
+    {
+        return preg_match('/^[A-Za-z0-9_\x4E00-\x9FA5-]{2,16}$/iu', $username);
+    }
+}
