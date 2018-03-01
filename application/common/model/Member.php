231f365a6eb16f336f82be61516788cd6c282502
mahaiyin <mahaiyin@qq.com>
2018-03-01 15:33:36 +0800

    member model

---
+++ application/common/model/Member.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-01
+ * Time: 10:50
+ */
+namespace app\common\model;
+
+use think\Model;
+
+class Member extends Model
+{
+    protected $pk = 'id';
+}
