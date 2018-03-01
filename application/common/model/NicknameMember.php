a6057ec806ea5590aab99d256ee356fe1d6b9971
mahaiyin <mahaiyin@qq.com>
2018-03-01 15:34:29 +0800

    nickname_member model

---
+++ application/common/model/NicknameMember.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-01
+ * Time: 11:00
+ */
+namespace app\common\model;
+
+use think\Model;
+
+class NicknameMember extends Model
+{
+    protected $pk = 'id';
+}
