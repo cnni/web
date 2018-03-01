c4353c89fbaf07866f063fb8a5861f13aeca115a
mahaiyin <mahaiyin@qq.com>
2018-03-01 16:24:41 +0800

    member

---
+++ application/index/controller/Member.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-01
+ * Time: 11:35
+ */
+namespace app\index\controller;
+
+use app\common\controller\Common;
+
+class Member extends Common
+{
+    public function index()
+    {
+        $this->assign([
+            'title' => '会员中心',
+            'keywords' => '会员中心',
+            'description' => '会员中心',
+        ]);
+        return $this->fetchs();
+    }
+}
