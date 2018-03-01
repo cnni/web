21ab9964fb01414b5a622e98b445158d73289809
mahaiyin <mahaiyin@qq.com>
2018-03-01 15:11:49 +0800

    model Sys

---
+++ application/common/model/Sys.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-01
+ * Time: 15:04
+ */
+namespace app\common\model;
+
+use think\Model;
+
+class Sys extends Model
+{
+    protected $pk = 'id';
+}
