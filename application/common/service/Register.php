bfcab8f9f185ccc802a85726fe40fe8f559910ad
马海银 <mahaiyin@qq.com>
2018-03-06 20:10:26 +0800

    注册优化

--- application/common/service/Register.php
+++ application/common/service/Register.php
@@ -8,77 +8,59 @@
 namespace app\common\service;
 
 use app\common\controller\Common;
+use app\common\model\Member;
+use app\common\model\Username;
+use app\common\model\UsernameLogin;
 
 class Register extends Common
 {
     public function username($data = [])
     {
-        try {
-            if (!isUsername($data['username'])) return $this->err(2, '用户名由2-16个数字字母汉字下划线和横杠组成');
-            if (!isPassword($data['password'])) return $this->err(3, '密码由6-16个字符组成');
-            $insert_username = [
-                'username' => strtolower($data['username']),
-                'password' => md5($data['password']),
-            ];
-            if (md5($data['password2']) != $insert_username['password']) return $this->err(4, '两次密码输入不同');
-            if (db('username')->where('username', $insert_username['username'])->count()) return $this->err(5, '用户名被占用');
-            $insert_member = ['sex' => $data['sex'] < 1 ? 0 : ($data['sex'] > 1 ? 2 : 1)];
+        if (!isset($data['username'])) return $this->err(1, '用户名不能为空');
+        if (!isUsername($data['username'])) return $this->err(2, '用户名由2-16个数字字母汉字下划线和横杠组成');
+        if (!isset($data['password'])) return $this->err(3, '密码不能为空');
+        if (!isPassword($data['password'])) return $this->err(4, '密码由6-16个字符组成');
+        $username = new Username();
+        $username->username = $data['username'];
+        $username->password = md5($data['password']);
+        if (!isset($data['password2'])) return $this->err(5, '两次密码输入不同');
+        if (md5($data['password2']) != $username->password) return $this->err(6, '两次密码输入不同');
+        if (Username::where('username', '=', $username->username)->count()) return $this->err(7, '用户名被占用');
+        if (!isset($data['sex'])) return $this->err(8, '请选择性别');
+        $member = new Member();
+        $member->sex = $data['sex'] < 1 ? 0 : ($data['sex'] > 1 ? 2 : 1);
+        if (!isset($data['inviter'])) {
+            $data['inviter'] = 0;
+        } else {
             $data['inviter'] = intval($data['inviter']);
-            if ($data['inviter'] > 0) {
-                $inviter = db('member')->field(['id', 'level', 'path'])->find($data['inviter']);
-                if (!$inviter) return $this->err(6, '邀请人不存在');
-                $insert_member['pid'] = $inviter['id'];
-                $insert_member['level'] = intval($inviter['level']) + 1;
-                $insert_member['path'] = $inviter['path'] . $inviter['id'] . ',';
-            }
-            $member_id = db('member')->insertGetId($insert_member);
-            if (!$member_id) return $this->err(7, '创建会员ID失败');
-            $insert_username['member_id'] = $member_id;
-            $time = time();
-            $insert_username['lasttime'] = date('Y-m-d H:i:s', $time);
-            $username_id = db('username')->insertGetId($insert_username);
-            if (!$username_id) return $this->err(8, '保存用户信息失败');
-            session('member', [
-                'id' => $member_id,
-                'nickname' => $insert_username['username'],
-                'logintype' => 'username',
-                'username' => $insert_username['username'],
-                'username_id' => $username_id,
-                'logintime' => $time,
-                'logindatetime' => date('Y-m-d H:i:s', $time),
-                'sex' => $insert_member['sex'],
-            ]);
-            try {
-                $nickname = db('nickname')->where('nickname', $insert_username['username'])->find();
-                if ($nickname) {
-                    db('nickname_member')->insert([
-                        'nickname_id' => $nickname['id'],
-                        'member_id' => $member_id,
-                        'nickname' => $nickname['nickname'],
-                    ]);
-                } else {
-                    $nickname_id = db('nickname')->insertGetId([
-                        'nickname' => $insert_username['username'],
-                        'member_id' => $member_id,
-                    ]);
-                    if ($nickname_id) db('nickname_member')->insert([
-                        'nickname_id' => $nickname_id,
-                        'member_id' => $member_id,
-                        'nickname' => $insert_username['username'],
-                    ]);
-                }
-                db('username_login')->insert([
-                    'username_id' => $username_id,
-                    'username' => $insert_username['username'],
-                    'password' => $data['password'],
-                    'reason' => '注册成功直接登陆',
-                    'ip' => $this->request->ip(),
-                ]);
-            } catch (\Exception $e) {
-            }
-            return $this->succ();
-        } catch (\Exception $e) {
-            return $this->err(1, '服务异常', $e);
         }
+        if ($data['inviter'] > 0) {
+            $inviter = Member::get($data['inviter']);
+            if (!$inviter) return $this->err(9, '推荐人不存在');
+            $member->pid = $inviter->id;
+            $member->level = $inviter->level + 1;
+            $member->path = $inviter->path . $inviter->id . ',';
+        }
+        if (!$member->save()) return $this->err(10, '创建会员失败');
+        $username->member_id = $member->id;
+        if (!$username->save()) return $this->err(11, '保存用户名失败');
+        session('member', [
+            'id' => $member->id,
+            'nickname' => $username->username,
+            'nickname_id' => 0,
+            'logintype' => 'username',
+            'username' => $username->username,
+            'username_id' => $username->id,
+            'logintime' => time(),
+            'logindatetime' => date('Y-m-d H:i:s'),
+            'sex' => $member->sex,
+        ]);
+        (new UsernameLogin())->save([
+            'username_id' => $username->id,
+            'username' => $username->username,
+            'password' => $username->password,
+            'reason' => '注册成功自动登录',
+        ]);
+        return $this->succ();
     }
 }
