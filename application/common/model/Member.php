d869b80d0a3d21c156a6766a5704bebf8b81335f
mahaiyin <mahaiyin@qq.com>
2018-03-06 10:44:09 +0800

    模型配置

--- application/common/model/Member.php
+++ application/common/model/Member.php
@@ -12,4 +12,8 @@ use think\Model;
 class Member extends Model
 {
     protected $pk = 'id';
+    protected $autoWriteTimestamp = 'datetime';
+    protected $createTime = 'addtime';
+    protected $updateTime = 'lasttime';
+    protected $readonly = ['pid', 'level', 'path'];
 }
