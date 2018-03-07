3a704a3305f845142c4fe6bdddf4a2bc18e5bb0f
mahaiyin <mahaiyin@qq.com>
2018-03-07 13:08:15 +0800

    附件服务器表

---
+++ application/common/model/FileServer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-07
+ * Time: 10:25
+ */
+namespace app\common\service;
+
+use think\Model;
+
+class FileServer extends Model
+{
+    protected $pk = 'id';
+    protected $autoWriteTimestamp = 'datetime';
+    protected $createTime = 'addtime';
+    protected $updateTime = 'lasttime';
+    protected $readonly = ['type', 'type_id'];
+}
