a8b90a6165c546907a7f4e39fcfdaa5430b5f4ad
mahaiyin <mahaiyin@qq.com>
2018-03-07 15:57:25 +0800

    UpYun缓存表/回掉表

---
+++ application/common/model/UpyunCache.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-07
+ * Time: 15:51
+ */
+namespace app\common\service;
+
+use think\Model;
+
+class UpyunCache extends Model
+{
+    protected $pk = 'id';
+    protected $autoWriteTimestamp = 'datetime';
+    protected $createTime = 'addtime';
+    protected $updateTime = 'lasttime';
+    protected $json = ['extend'];
+    protected $readonly = ['member_id', 'filename', 'md5', 'url', 'upyun_id', 'server_id', 'extend'];
+}
