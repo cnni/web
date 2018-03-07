471bf4c50045511c890678264fa1ad0546d173e5
mahaiyin <mahaiyin@qq.com>
2018-03-07 17:22:43 +0800

    头像表

---
+++ application/common/model/Avatar.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haiyin.ma
+ * Date: 2018-03-07
+ * Time: 17:18
+ */
+namespace app\common\model;
+
+use think\Model;
+
+class Avatar extends Model
+{
+    protected $pk = 'id';
+    protected $autoWriteTimestamp = 'datetime';
+    protected $createTime = 'addtime';
+    protected $updateTime = 'lasttime';
+    protected $readonly = ['member_id', 'file_id'];
+}
